1d3b7c52fbe96549e0abdf5788bacd21e2dbc1f0
xiaojunfeng <xiaojunfeng@sjtu.edu.cn>
2025-04-23 23:49:04 +0800

    feat: UI position

--- Assets/Script/StartMenu/StartMenu.cs
+++ Assets/Script/StartMenu/StartMenu.cs
@@ -3,8 +3,36 @@ using UnityEngine.SceneManagement;
 
 public class StartMenu : MonoBehaviour
 {
+    public RectTransform title;
+    public RectTransform logo;
+    public RectTransform startButton;
+    public RectTransform exitButton;
+    void Start()
+    {
+        float screenWidth = Screen.width;
+        float screenHeight = Screen.height;
+        title.sizeDelta = new Vector2(screenWidth * 0.5f, screenHeight * 0.2f);
+        title.anchorMin = new Vector2(0.5f, 0.5f);
+        title.anchorMax = new Vector2(0.5f, 0.5f); 
+        title.anchoredPosition = new Vector2(0, screenHeight * 0.1f);
 
+        logo.sizeDelta = new Vector2(screenHeight * 0.15f, screenHeight * 0.15f);
+        logo.anchorMin = new Vector2(1, 1);
+        logo.anchorMax = new Vector2(1, 1); 
+        logo.anchoredPosition = new Vector2(-screenHeight * 0.1f, -screenHeight * 0.1f);
 
+        startButton.sizeDelta = new Vector2(screenWidth * 0.3f, screenHeight * 0.1f);
+        startButton.anchorMin = new Vector2(0.5f, 0.5f);
+        startButton.anchorMax = new Vector2(0.5f, 0.5f); 
+        startButton.anchoredPosition = new Vector2(0, 0);
+
+        exitButton.sizeDelta = new Vector2(screenWidth * 0.3f, screenHeight * 0.1f);
+        exitButton.anchorMin = new Vector2(0.5f, 0.5f);
+        exitButton.anchorMax = new Vector2(0.5f, 0.5f); 
+        exitButton.anchoredPosition = new Vector2(0, -screenHeight * 0.15f);
+    }
+    void Update()
+    {}
     public void StartGame()
     {
         
--- Assets/Script/UI/PauseMenu.cs
+++ Assets/Script/UI/PauseMenu.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
-
+        public RectTransform startButton;
+    public RectTransform exitButton;
     public ESChandler esc;
     // Start is called before the first frame update
 
@@ -23,7 +24,17 @@ public class PauseMenu : MonoBehaviour
     
     void Start()
     {
+                float screenWidth = Screen.width;
+        float screenHeight = Screen.height;
+                startButton.sizeDelta = new Vector2(screenWidth * 0.3f, screenHeight * 0.1f);
+        startButton.anchorMin = new Vector2(0.5f, 0.5f);
+        startButton.anchorMax = new Vector2(0.5f, 0.5f); 
+        startButton.anchoredPosition = new Vector2(0, screenHeight * 0.15f);
 
+        exitButton.sizeDelta = new Vector2(screenWidth * 0.3f, screenHeight * 0.1f);
+        exitButton.anchorMin = new Vector2(0.5f, 0.5f);
+        exitButton.anchorMax = new Vector2(0.5f, 0.5f); 
+        exitButton.anchoredPosition = new Vector2(0, 0);
     }
     void Update(){
         if (Input.GetKeyDown(KeyCode.Escape))
